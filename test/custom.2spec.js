b8aecc54b88c59a0cbcdcee488a857e4f617c96e
Eugene Korzun <e.korzun@gmail.com>
2019-04-24 18:13:21 +0300

    custom2 test

---
+++ test/custom.2spec.js
@@ -0,0 +1,63 @@
+import { expect } from 'chai'
+import './helpers/setconfig'
+
+import { Model } from '../src'
+
+const User = new Model({
+  name: 'User',
+  api: {
+    myget: {
+      url: '/users/:id',
+      store: 'myusers',
+      export: (data = {}) => {
+        return {
+          ...data,
+          // id: data.my_user_id
+        }
+      }
+    },
+  }
+})
+
+describe('Advanced', () => {
+  
+  it('Custom request method', done => {
+    User.myget({
+      id:1
+    })
+    .catch(e => {
+      console.log(e)
+      done()
+    })
+    .then(user => {
+      expect(user.id).to.equal(1)
+      done()
+    })
+  })
+
+  // it('Custom request method should be cached', done => {
+  //   User.myget({
+  //     id: 1
+  //   }).then(user => {
+  //     expect(user.id).to.equal(1)
+  //     done()
+  //   })
+  // })
+
+
+  it('custom data export', done => {
+    User.myget({
+      my_user_id: 1
+    })
+    .catch(err => {
+      console.error(err)
+      done()
+    })
+    .then(user => {
+      // console.log('user: ', user);
+      expect(user.id).to.equal(1)
+      done()
+    })
+  })
+
+})
